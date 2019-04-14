52be597c5fa98670a93f900ea748fa618f59fde7
Peter Strömberg <pez@pezius.com>
2019-04-14 20:52:02 +0200

    Add reagent and stuff

--- src/starter/browser.cljs
+++ src/starter/browser.cljs
@@ -1,12 +1,34 @@
-(ns starter.browser)
+(ns starter.browser
+  (:require [reagent.core :as r]))
+
+(defonce fibby (r/atom [0 1]))
+
+(defn next-fib [a b]
+  (let [sum (+ a b)]
+    [b sum]))
+
+(defn fib-component []
+  [:div
+   [:p "Current fib: "]
+   [:h3 (first @fibby)]
+   [:input {:type "button" :value "Click me!"
+            :on-click #(swap! fibby (partial apply next-fib))}]])
+
+
+(defn body []
+  [:div  
+   [:h1 "Hello Reagent World"]
+   [:p "A counter example 😀"]
+   [fib-component]])
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
-  (js/console.log "start"))
+  (js/console.log "start")
+  (r/render [body] (js/document.getElementById "app")))
 
 (defn ^:export init []
   ;; init is called ONCE when the page loads
-  ;; this is called in the index.html and must be exported
+  ;; it is called in the index.html and must be exported
   ;; so it is available even in :advanced release builds
   (js/console.log "init")
   (start))
